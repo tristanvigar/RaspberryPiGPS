5bc37e528587a3572b000364897104d3a4488944
Tristan Vigar <tristanvigar@protonmail.com>
2019-11-29 21:59:04 -0700

    Initial commit of RaspberryPiGPS files

---
+++ gps_handler.py
@@ -0,0 +1,20 @@
+class GPS:
+    def__init__(self, uart):
+
+# Class definitions:
+
+# Time (UTC)
+# Latitude
+# N / S
+# Longitude
+# E / W
+# GPS Fix Quality
+# Num of satellites in view
+# Horizontal Dilution of Precision (DOP)
+# Antenna altitude above/below mean-sea-level (geoid)
+# Units of Antenna Altitude, Meters
+# geoidal separation, difference between WGS084 earth ellipsoid and mean-sea-level (geoid), "-" means mean-sea-level below ellipsoid
+# Units of geoidal separation, Meters
+# Age of differential GPS, data, time in seconds since last SC104 type 1 or 9 update, null when DPGS is not used
+# Differential reference station ID, 0000-1023
+# Checksum
---
+++ sample_gps_data.py
@@ -0,0 +1,21 @@
+import time
+import serial
+
+sample_data_write_path = '/home/pi/Development/PiGPS/sample_gps_output.txt'
+
+uart = serial.Serial('/dev/ttyAMA0', baudrate=9600, timeout=10)
+
+if not uart:
+    import sys
+    print('Could not find /dev/ttyAMA0')
+    sys.exit()
+
+counter = 0
+
+# Write 50 lines of sample data
+with open(sample_data_write_path, 'a') as sample_data_file:
+    while counter <= 50:
+        gps_message = uart.readline()
+        print(gps_message)
+        sample_data_file.write(str(gps_message) + '\n')
+        counter += 1
